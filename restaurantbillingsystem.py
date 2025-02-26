02780ccd58fc3655c7c013cd19fe93c18a7e031d
NavyaAbrol23 <navyaabrol.23@gmail.com>
2025-02-27 03:01:23 +0530

    Update restaurantbillingsystem.py

--- restaurantbillingsystem.py
+++ restaurantbillingsystem.py
@@ -5,7 +5,7 @@ def connect_db():
     return mysql.connector.connect(
         host="localhost",
         user="navya",
-        password="navyasqlconnect",
+        password="*************",
         database="restaurant_db"
     )
 
